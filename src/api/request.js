fb93b91de07ac62662e8e4c31c6ce2d90e155cb4
mitex22 <mitex22@abv.bg>
2026-02-20 19:02:44 +0200

    add api location for iOS as well

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.4:3001',
+    // baseURL: 'http://192.168.1.4:3001',
+    baseURL: 'http://192.168.33.106:3001',
     headers: {
         'Content-Type': 'application/json',
     },
